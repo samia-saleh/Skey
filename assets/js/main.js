c52c9566325174a7e3f19acf25463ef5663c9070
Samia Saleh <salehsamia869@gmail.com>
2025-04-27 05:52:04 +0300

    2nd commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function openNav() {
+function toggle() {
     if(document.getElementById("Sidebar").style.display==="none" || document.getElementById("Sidebar").style.display===""){
          document.getElementById("Sidebar").style.display="flex";
          document.getElementById("toggle").className="fa-solid fa-xmark";
@@ -9,19 +9,9 @@ function openNav() {
         document.getElementById("toggle").className="fa-solid fa-bars";
     }
 
-    // alert("ff")
- 
-//   document.getElementById("openbtn").style.display="none";
-//   document.getElementById("closebtn").style.display="flex";
-//   document.getElementById("main").style.marginLeft = "250px";
 }
 
-function closeNav() {
-    document.getElementById("Sidebar").style.display="none";
-    document.getElementById("closebtn").style.display="none";
-    document.getElementById("openbtn").style.display="flex";
-//   document.getElementById("main").style.marginLeft= "0";
-}
+
 window.addEventListener('scroll', () => {
 const navbar = document.getElementById('header');
 if (window.scrollY > 10) {
